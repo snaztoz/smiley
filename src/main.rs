e6a982e8c92c1ffc8c47f52fb65b91e0319312bd
snaztoz <hafidhmn@gmail.com>
2022-01-10 10:21:36 +0700

    Create basic CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+/// A (yet-another) simple CSS preprocessor
+#[derive(Parser, Debug)]
+#[clap(about, version)]
+struct Cli {
+    /// Root source file
+    src: PathBuf,
+
+    /// Run smiley in `watch` mode
+    #[clap(short, long)]
+    watch: bool,
+
+    /// Specify the output file path
+    #[clap(short, long)]
+    out: Option<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("{:?}", cli);
 }
