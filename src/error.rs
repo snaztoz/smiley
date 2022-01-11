c355654b9c8ac3ef34fbd93b739ced71293b207f
snaztoz <hafidhmn@gmail.com>
2022-01-11 22:58:32 +0700

    Refactor

--- src/error.rs
+++ src/error.rs
@@ -5,7 +5,16 @@ pub fn report(file: &Path, row: usize, col: usize, message: &str) {
     let content = fs::read_to_string(file).unwrap();
     let line = content.lines().nth(row - 1).unwrap();
 
-    let err_line = format!("{}:{}:{}\n\n\t`{}`\n", file.display(), row, col, line);
+    let location = format!("{}:{}:{}", file.display(), row, col);
+    let err_report = format!(
+        "\
+    {}
+    |
+    |   `{}`
+    |
+    ",
+        location, line
+    );
 
-    error!("{}\n\n\t{}", message, err_line);
+    error!("{}\n\n\t{}", message, err_report);
 }
