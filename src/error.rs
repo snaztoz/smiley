f746c53d5795c0ef4dd0a02a4fff59baabdabeef
snaztoz <hafidhmn@gmail.com>
2022-01-24 22:04:55 +0700

    Refactor

--- src/error.rs
+++ src/error.rs
@@ -2,7 +2,7 @@ use crate::preprocessor::{
     indentation::ErrorKind as IndentationErrorKind,
     line::{Col, Row},
 };
-use indoc::indoc;
+use indoc::{formatdoc, indoc};
 use log::error;
 use std::{fs, path::Path};
 
@@ -11,25 +11,25 @@ pub fn report(file: &Path, row: Row, col: Col, message: &str) {
     let line = content.lines().nth(row - 1).unwrap();
 
     let location = format!("{}:{}:{}", file.display(), row, col);
-    let err_report = format!(
-        "\
-    {}
-    |
-    |   `{}`
-    |
-    ",
-        location,
-        line.escape_default()
-    );
+    let escaped_line = line.escape_default();
 
-    error!("{}\n\t{}", message, err_report);
+    let err_report = formatdoc! {"
+        {location}
+        |
+        |   `{escaped_line}`
+        |
+    "};
+
+    error!("{message}\n{err_report}");
 }
 
 pub fn report_indentation_error(file: &Path, kind: IndentationErrorKind, row: Row, col: Col) {
     let msg = match kind {
         IndentationErrorKind::InconsistentIndentation => indoc! {"
-            Inconsistent indentation: Smiley files should only use either
-            \tspace or tab as indentation character, but never both
+            Inconsistent indentation
+
+            Smiley files should only use either space or tab as
+            indentation character, but never both
         "},
         IndentationErrorKind::UnexpectedIndentation => indoc! {"
             Unexpected indentation
