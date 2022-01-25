9b67ee36fde82a256d7ede731421f804b9667f18
snaztoz <hafidhmn@gmail.com>
2022-01-25 13:41:06 +0700

    Refactor

---
+++ src/parser/selector.rs
@@ -0,0 +1,38 @@
+#[derive(Parser)]
+#[grammar = "grammar/selector.pest"]
+pub struct SelectorParser;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pest::{self, consumes_to};
+
+    #[test]
+    fn basic_selector_parsing() {
+        let cases = [
+            "foo",
+            "foo.bar",
+            "foo.bar.bat",
+            "foo#bar.bat",
+            ".foo.bar.bat",
+            "foo.bar[ baz |= 'string' i ].abc",
+            "[foo=bar i][ baz ~= \"bat\" S]",
+        ];
+
+        for case in &cases {
+            let eoi_pos = case.len();
+
+            pest::parses_to! {
+                parser: SelectorParser,
+                input: case,
+                rule: Rule::selector,
+                tokens: [
+                    selector(0, eoi_pos, [
+                        basic_selector(0, eoi_pos),
+                        EOI(eoi_pos, eoi_pos),
+                    ]),
+                ]
+            };
+        }
+    }
+}
