457a5b79a2ec899d741a06656d4592475e365cd4
snaztoz <hafidhmn@gmail.com>
2022-01-26 21:25:39 +0700

    Move parser back to single file

--- src/parser.rs
+++ src/parser.rs
@@ -1,1 +1,39 @@
-mod selector;
+#[derive(Parser)]
+#[grammar = "grammar/smiley.pest"]
+pub struct SmileyParser;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pest::{self, consumes_to};
+
+    #[test]
+    fn basic_selector_parsing() {
+        let cases = [
+            "foo",
+            "foo.bar",
+            "foo.bar.bat",
+            "foo#bar.bat",
+            ".foo.bar.bat",
+            "foo.bar[ baz |= 'string' i ].abc",
+            "[foo=bar i][ baz ~= \"bat\" S]",
+            ".foo-bar.bat[abc = ghi]",
+        ];
+
+        for case in &cases {
+            let eoi_pos = case.len();
+
+            pest::parses_to! {
+                parser: SmileyParser,
+                input: case,
+                rule: Rule::selector,
+                tokens: [
+                    selector(0, eoi_pos, [
+                        basic_selector(0, eoi_pos),
+                        EOI(eoi_pos, eoi_pos),
+                    ]),
+                ]
+            };
+        }
+    }
+}
